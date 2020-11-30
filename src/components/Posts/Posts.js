9438b2b69bc5a702118eca12187af4627a206773
lex-marie790 <lexi.marie790@gmail.com>
2020-11-29 21:10:02 -0800

    posts.js completed

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -10,6 +10,18 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map(post => {
+        return (
+          <Post
+            key = {post.id}
+            post = {post}
+            likePost = {likePost}
+            post = {post}
+            comments = {posts.comments}
+            numberLikes = {post.likes}
+          />
+        )
+      })}
     </div>
   );
 };
